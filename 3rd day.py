bab1ffa76fad0be65958b57755384f9784c8ef69
SHAIK KHASIM  BASHA <115825218+khasimbasha5757@users.noreply.github.com>
2023-12-13 15:05:56 +0530

    Add files via upload

---
+++ 3rd day.py
@@ -0,0 +1,20 @@
+# # variables
+# a=1934087635632243333335
+# print(a)
+# b="Harry"
+# print(b)
+a=1
+b=False
+c="HArry"
+d=None
+print(a+b)
+print("Here we can know that the type of a is ",type(a))
+print("Here we can know that the type of b is ",type(b))
+print("Here we can know that the type of c is ",type(c))
+print("Here we can know that the type of d is ",type(d))
+
+
+
+# builtin datatypes
+a=complex(1,2);
+print(a)
